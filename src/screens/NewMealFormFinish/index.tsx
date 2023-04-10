e0b2dd8c47fc4421fb365ebb97b44015b22f51fd
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2023-04-10 20:16:46 -0300

    feat: NewMealFormFinish

--- src/screens/NewMealFormFinish/index.tsx
+++ src/screens/NewMealFormFinish/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import {
   useNavigation,
   CommonActions,
@@ -8,6 +8,8 @@ import {
 import * as S from './styles'
 import { MealStatus } from '../../@types/global'
 import { Button } from '@components/Button'
+import SvgIn from '@assets/Illustration1.svg'
+import SvgOut from '@assets/Illustration2.svg'
 
 type RouteParams = {
   status: MealStatus
@@ -18,7 +20,30 @@ export const NewMealFormFinish = () => {
   const route = useRoute()
   const routeParams = route.params as RouteParams
 
-  console.log('>>> status', routeParams.status)
+  const isStatusIn = routeParams.status === MealStatus.IN
+
+  const data = useMemo(() => {
+    return isStatusIn
+      ? {
+          title: 'Continue assim!',
+          subtitle: (
+            <S.Text>
+              Você continua <S.Text isBold>dentro da dieta</S.Text>. Muito bem!
+            </S.Text>
+          ),
+          illustration: <SvgIn />
+        }
+      : {
+          title: 'Que pena!',
+          subtitle: (
+            <S.Text>
+              Você <S.Text isBold>saiu da dieta</S.Text> dessa vez, mas continue
+              se esforçando e não desista!
+            </S.Text>
+          ),
+          illustration: <SvgOut />
+        }
+  }, [isStatusIn])
 
   const goToHome = useCallback(() => {
     navigation.dispatch(
@@ -31,6 +56,12 @@ export const NewMealFormFinish = () => {
 
   return (
     <S.Wrapper>
+      <S.Title isIn={isStatusIn}>{data.title}</S.Title>
+
+      {data.subtitle}
+
+      {data.illustration}
+
       <Button text="Ir para a página inicial" onPress={goToHome} />
     </S.Wrapper>
   )
--- src/screens/NewMealFormFinish/styles.ts
+++ src/screens/NewMealFormFinish/styles.ts
@@ -1,8 +1,39 @@
 import styled, { css } from 'styled-components/native'
 
-export const Wrapper = styled.SafeAreaView`
+type TitleProps = {
+  isIn: boolean
+}
+
+type TextProps = {
+  isBold?: boolean
+}
+
+export const Wrapper = styled.View`
   ${({ theme }) => css`
-    flex: 1;
+    flex-grow: 1;
+    justify-content: center;
+    align-items: center;
+    gap: 24px;
+    padding: 24px;
     background-color: ${theme.colors.white};
+  `}
+`
+
+export const Title = styled.Text<TitleProps>`
+  ${({ theme, isIn }) => css`
+    font-family: ${theme.fonts.family.nunitoSans.bold};
+    ${theme.fonts.size(24)};
+    color: ${isIn ? theme.colors.greenDark : theme.colors.redDark};
+  `}
+`
+
+export const Text = styled.Text<TextProps>`
+  ${({ theme, isBold }) => css`
+    font-family: ${isBold
+      ? theme.fonts.family.nunitoSans.bold
+      : theme.fonts.family.nunitoSans.regular};
+    ${theme.fonts.size(16)};
+    color: ${theme.colors.gray1};
+    text-align: center;
   `}
 `
